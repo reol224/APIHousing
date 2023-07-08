7bdbe7c9756991144c126bab6b8752e457cce1b9
Iulia Danilov <tmdoppelganger@gmail.com>
2023-07-08 18:45:18 -0400

    Error controller and default endpoint

--- src/main/java/com/conestoga/APIHousing/ApiHousingApplication.java
+++ src/main/java/com/conestoga/APIHousing/ApiHousingApplication.java
@@ -2,12 +2,23 @@ package com.conestoga.APIHousing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiHousingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiHousingApplication.class, args);
+	}
+
+	@RestController
+	public static class HomeController {
+
+		@RequestMapping("/")
+		public String home() {
+			return "Welcome to the API Housing application!";
+		}
 	}
 
 }
---
+++ src/main/java/com/conestoga/APIHousing/controller/CustomErrorController.java
@@ -0,0 +1,16 @@
+package com.conestoga.APIHousing.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "An error occurred.";
+    }
+}
+
+
+

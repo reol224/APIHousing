2acc7dca7d2e14c7afb699e248f21782f3601516
Nikkon <nikkon404@gmail.com>
2023-08-05 20:24:12 +0000

    minor fix

--- src/main/java/com/conestoga/APIHousing/configs/chat/WebSocketConfig.java
+++ src/main/java/com/conestoga/APIHousing/configs/chat/WebSocketConfig.java
@@ -9,15 +9,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final ChatWebSocketHandler chatWebSocketHandler;
 
-    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
-        this.chatWebSocketHandler = chatWebSocketHandler;
-    }
-
+ 
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(ChatWebSocketHandler.getInstance(), "/chat").setAllowedOrigins("*");
     }
 }
